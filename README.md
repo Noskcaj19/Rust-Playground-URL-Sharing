0778e60e34834ab7a68183bf10e968000994d8bd
Noskcaj <kcajdev@gmail.com>
2017-09-09 22:29:33 -0400

    Inital Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rust Playground URL Sharing
+
+A userscript to add sharing code in URLs to the [Rust Playground](https://play.rust-lang.org/).
+
+### Installation
+
+You can run this userscript by installing [Tampermonkey](https://tampermonkey.net), a Userscript extension that supports *tons* of browser and probably yours. Just go [here](https://github.com/Noskcaj19/Rust-Playground-URL-Sharing/blob/master/rust_playground.user.js), and either A) Click "raw" and it should prompt you to install it, or B) copy + paste the code into the "Add New Script" area.
+
+If you use Google Chrome, you _may_ be able to install the userscript without Tampermonkey by going to `chrome://extensions` and dragging `rust_playground.user.js` to the list.
+
+For Firefox users, [Greasemonkey](https://github.com/greasemonkey/greasemonkey) may work also.
---
+++ rust_playground.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Rust Playground URL sharing
+// @version      0.1
+// @description  Export code in URL from Rust Playground
+// @author       Noskcaj
+// @match        *://play.rust-lang.org/*
+// ==/UserScript==
+
+// Thanks MDN
+function fixedEncodeURIComponent(str) {
+    return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
+        return '%' + c.charCodeAt(0).toString(16);
+    }).replace(/%20/g, "+");
+}
+
+function addCodeParam(code) {
+    // Removing existing code component
+    var search = window.location.search.replace(/^\?/, '').replace(/code=.*?(?=&|$)/g, '');
+    var newSearch = "?";
+    if (search === "") {
+        newSearch += "code=" + code;
+    } else {
+        var key = /&$/.test(search) ? "code=" : "&code=";
+        newSearch += search + key + code;
+    }
+    window.history.pushState('obj', '', newSearch);
+}
+
+(function() {
+    'use strict';
+    var shareGroup = document.getElementsByClassName("header-set__buttons")[2];
+    var legend = document.createElement("legend");
+    legend.className = "header-set__title";
+    legend.innerText = "Sharing";
+    shareGroup.appendChild(legend);
+
+    var urlButton = document.createElement("button");
+    urlButton.className = "header-set__btn";
+    urlButton.innerText = "URL";
+    shareGroup.appendChild(urlButton);
+
+    urlButton.onclick = function save() {
+        var code = JSON.parse(localStorage.getItem('redux')).code;
+        var urlEncoded = fixedEncodeURIComponent(code);
+        addCodeParam(urlEncoded);
+    };
+})();
